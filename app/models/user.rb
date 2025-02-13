c17ffae9f5910aa0b772c7e2106e5c9ffffe25c3
taakaa0923 <napoyama3sei@gmail.com>
2025-02-13 21:24:34 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ンヴー]+\z/, message: 'カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
