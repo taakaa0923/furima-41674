833acfc342481742eda88fd94226ccd4dc0eef46
taakaa0923 <napoyama3sei@gmail.com>
2025-03-27 11:59:33 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:new, :create, :index]
+  before_action :set_item, only: [:create, :index]
   before_action :move_to_index
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_20_065928) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_23_121035) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,15 +40,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_065928) do
   end
 
   create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
-    t.string "postal_code", null: false
+    t.bigint "order_id", null: false
+    t.string "postal", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "house_number", null: false
     t.string "building_name"
     t.string "phone_number", null: false
-    t.bigint "order_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "prefecture_id"
     t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
@@ -81,6 +81,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_065928) do
   end
 
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "orderss", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "item_id"
@@ -109,4 +118,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_20_065928) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
