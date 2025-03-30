8f0b99883038f58f39c1e404041909acf37ccdf2
taakaa0923 <napoyama3sei@gmail.com>
2025-03-30 15:05:36 +0900

    修正

--- app/models/sales_status.rb
+++ app/models/sales_status.rb
@@ -3,7 +3,7 @@ class SalesStatus < ActiveHash::Base
   has_one :item
 
   self.data = [
-    { id: 0, name: '--' },
+    { id: 0, name: '---' },
     { id: 1, name: '新品・未使用' },
     { id: 2, name: '未使用に近い' },
     { id: 3, name: '目立った傷や汚れなし' },
